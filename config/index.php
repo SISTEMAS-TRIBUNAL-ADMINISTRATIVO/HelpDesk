49970ae0e2d7735e1cbdae76adc0c1bc4c2c1101
SISTEMAS TRIBUNAL ADMINISTRATIVO <sistemastapje@hotmail.com>
2025-01-09 14:26:51 -0600

    Se elimina lo anterior para subir la version funcional

--- config/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-  //Cadena de conexion 
-  require_once("../config/conexion.php"); 
-  //Ruta Login
-  $conexion = new Conectar(); // Crear una instancia de la clase Conectar
-  header("Location:" . $conexion->ruta() . "index.php");
-?>
--- view/Logout/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once("../../config/conexion.php");
-    $conexion = new Conectar(); // Crear una instancia de la clase Conectar
-    session_destroy();
-    header("Location:". $conexion->ruta() . "index.php"); // Llamar al método ruta() mediante la instancia
-    exit();
-?>
